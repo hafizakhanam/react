5472451db54eaf4a5192ad286d1c98af8d14bc9c
Rima <hafiza@instabd.com>
2020-12-09 01:55:05 +0600

    Updates

---
+++ precache-manifest.6fd986aced54a17a01a018cddcc529ea.js
@@ -0,0 +1,66 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f0e0dabee689e7b67c57cb3ffedc48a9",
+    "url": "/react-rima/index.html"
+  },
+  {
+    "revision": "55e60bae92276de2da67",
+    "url": "/react-rima/static/css/2.1af7a3c5.chunk.css"
+  },
+  {
+    "revision": "2c46dda8eca79c6a53eb",
+    "url": "/react-rima/static/css/main.0a042481.chunk.css"
+  },
+  {
+    "revision": "55e60bae92276de2da67",
+    "url": "/react-rima/static/js/2.c23e3e93.chunk.js"
+  },
+  {
+    "revision": "2c46dda8eca79c6a53eb",
+    "url": "/react-rima/static/js/main.1dda9b8d.chunk.js"
+  },
+  {
+    "revision": "e39bc45d0c91f0559e84",
+    "url": "/react-rima/static/js/runtime-main.8f12df30.js"
+  },
+  {
+    "revision": "7165c11a6d8eb5b7603ff1e55e0d7d72",
+    "url": "/react-rima/static/media/banner.7165c11a.jpg"
+  },
+  {
+    "revision": "b4bc2f8e803744b47674b360abfc9bf7",
+    "url": "/react-rima/static/media/course-1.b4bc2f8e.jpg"
+  },
+  {
+    "revision": "a4f27fe2e0bc48409bbd8657dedf30ad",
+    "url": "/react-rima/static/media/course-2.a4f27fe2.jpg"
+  },
+  {
+    "revision": "e8a1cf66cf4b38c1cc16e51777dadf8a",
+    "url": "/react-rima/static/media/course-3.e8a1cf66.jpg"
+  },
+  {
+    "revision": "67e319fce7d04834a0c4d8bb2a11df17",
+    "url": "/react-rima/static/media/course-4.67e319fc.jpg"
+  },
+  {
+    "revision": "2a87eedaaa18e483ba04f0e565df1a1f",
+    "url": "/react-rima/static/media/graphics.2a87eeda.svg"
+  },
+  {
+    "revision": "704e74849e80be9cdc356d5f1c2ea050",
+    "url": "/react-rima/static/media/mobile.704e7484.svg"
+  },
+  {
+    "revision": "f599bd96d0ddf7dcec79676b24f20962",
+    "url": "/react-rima/static/media/navlogo.f599bd96.svg"
+  },
+  {
+    "revision": "d6db46a970f10d7612213d5e4ed72ed7",
+    "url": "/react-rima/static/media/navlogoScroll.d6db46a9.svg"
+  },
+  {
+    "revision": "3515765f8235b5f2fe302334ba570cc5",
+    "url": "/react-rima/static/media/web.3515765f.svg"
+  }
+]);
